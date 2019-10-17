07b389432096d1bf552b44c92361fcf9752306a1
Christopher Edwards <cedwards036@gmail.com>
2019-10-17 15:15:55 -0400

    Add class to transform raw response values based on a given mapping

---
+++ src/transform/value_replacer.py
@@ -0,0 +1,17 @@
+class ValueReplacer:
+
+    def __init__(self):
+        self._mapping = {}
+
+    def add_mapping(self, value, replacement):
+        self._mapping[value] = replacement
+
+    def replace(self, value):
+        try:
+            return self._mapping[value]
+        except KeyError:
+            raise self.NoKnownMappingException(f'No known mapping for value "{value}"')
+
+    class NoKnownMappingException(Exception):
+        def __init__(self, message):
+            super().__init__(message)
