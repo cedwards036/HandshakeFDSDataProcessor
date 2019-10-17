b6a7f8a70558924afac04e430b84872083134bd8
Christopher Edwards <cedwards036@gmail.com>
2019-10-17 15:05:55 -0400

    Add Survey Response class

---
+++ test/test_survey_response.py
@@ -0,0 +1,26 @@
+import unittest
+
+from src.survey_response import SurveyResponse
+
+
+class TestSurveyResponse(unittest.TestCase):
+
+    def test_all_internships_count_updates_automatically(self):
+        test_response = SurveyResponse()
+        self.assertIsNone(test_response.all_internships_count)
+        test_response.unpaid_internships_count = 2
+        self.assertEqual(2, test_response.all_internships_count)
+        test_response.paid_internships_count = 3
+        self.assertEqual(5, test_response.all_internships_count)
+        test_response.unpaid_internships_count = 10
+        self.assertEqual(13, test_response.all_internships_count)
+
+    def test_all_research_count_updates_automatically(self):
+        test_response = SurveyResponse()
+        self.assertIsNone(test_response.all_research_count)
+        test_response.unpaid_research_count = 2
+        self.assertEqual(2, test_response.all_research_count)
+        test_response.paid_research_count = 3
+        self.assertEqual(5, test_response.all_research_count)
+        test_response.unpaid_research_count = 10
+        self.assertEqual(13, test_response.all_research_count)
