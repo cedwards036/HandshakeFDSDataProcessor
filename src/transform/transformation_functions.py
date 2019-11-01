4acbc7bbda3991a4fcda90931c5b8263e7d60918
Christopher Edwards <cedwards036@gmail.com>
2019-11-01 10:57:08 -0400

    Refactor transform method so that it delegates to helper classes

--- src/transform/transformation_functions.py
+++ src/transform/transformation_functions.py
@@ -6,25 +6,44 @@ from src.transform.location_map import build_location_map
 
 
 def transform_2019_fds_data(dataset: ResponseDataset, mapping_filepaths: dict) -> ResponseDataset:
-    location_map = _get_location_map(mapping_filepaths['location'])
-    employer_name_map = _get_employer_name_map(mapping_filepaths['employer_name'])
-    response: SurveyResponse
+    mappings = Mappings(mapping_filepaths)
     for response in dataset:
-        response.metadata.location = location_map.get_mapping(response.metadata.location)
-        response.employment.employer_name = employer_name_map.get_mapping(response.employment.employer_name)
-        _set_is_jhu(response)
+        ResponseCleaner(mappings, response).clean()
     return dataset
 
 
-def _set_is_jhu(response):
-    response.metadata.is_jhu = is_jhu(response.cont_ed.school) or is_jhu(response.employment.employer_name)
+class Mappings:
+
+    def __init__(self, mapping_filepaths: dict):
+        self._mapping_filepaths = mapping_filepaths
+        self.location_map = self._get_location_map()
+        self.employer_name_map = self._get_employer_name_map()
+
+    def _get_location_map(self):
+        raw_data = csv_to_list_of_dicts(self._mapping_filepaths['location'])
+        return build_location_map(raw_data)
+
+    def _get_employer_name_map(self):
+        raw_data = csv_to_list_of_dicts(self._mapping_filepaths['employer_name'])
+        return build_value_map(raw_data, 'old_value', 'new_value')
 
 
-def _get_location_map(filepath: str):
-    raw_data = csv_to_list_of_dicts(filepath)
-    return build_location_map(raw_data)
+class ResponseCleaner:
 
+    def __init__(self, mappings: Mappings, response: SurveyResponse):
+        self._response = response
+        self._mappings = mappings
 
-def _get_employer_name_map(filepath: str):
-    raw_data = csv_to_list_of_dicts(filepath)
-    return build_value_map(raw_data, 'old_value', 'new_value')
+    def clean(self):
+        self._clean_locations()
+        self._set_is_jhu()
+        self._clean_employer_names()
+
+    def _clean_locations(self):
+        self._response.metadata.location = self._mappings.location_map.get_mapping(self._response.metadata.location)
+
+    def _set_is_jhu(self):
+        self._response.metadata.is_jhu = is_jhu(self._response.cont_ed.school) or is_jhu(self._response.employment.employer_name)
+
+    def _clean_employer_names(self):
+        self._response.employment.employer_name = self._mappings.employer_name_map.get_mapping(self._response.employment.employer_name)
